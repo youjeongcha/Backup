7fe18e7cef6a9b670bcad01edf648ca0ccaefa9b
youjeongcha <97075247+youjeongcha@users.noreply.github.com>
2024-01-03 18:59:28 +0900

    TCP_IP_Client_Non-blockingSocket

---
+++ TCP_IP_Client_Non-blockingSocket/TCP_IP_Client_Non-blockingSocket.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "resource.h"
+
+SOCKET g_sock;
+std::string g_strChat;
+std::vector<std::string> g_vLog;
+
+void AddLog(const char* str);
+void ErrorQuit(const char* caption);
+void ErrorDisplay(HWND hWnd, const char* caption);
+void ErrorDisplay(HWND hWnd, const int& errorCode, const char* caption);
+void SocketProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
